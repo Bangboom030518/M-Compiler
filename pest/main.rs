bfb969e0041b065a38d576a55b9329d43e5d06bf
Charlie Clift <bang.boom030518@gmail.com>
2022-08-22 18:15:01 +0100

    Add import statements to grammar

--- pest/main.rs
+++ pest/main.rs
@@ -1,51 +1,18 @@
 #[macro_use]
 extern crate pest_derive;
 
-use pest::iterators::{Pair, Pairs};
-use pest::prec_climber::{Assoc, Operator, PrecClimber};
+// use pest::iterators::{Pair, Pairs};
+// use pest::prec_climber::{Assoc, Operator, PrecClimber};
 use pest::Parser;
-use lazy_static::lazy_static;
 
 #[derive(Parser)]
-#[grammar = "../pest/calculator.pest"]
-pub struct CalculatorParser;
-
-lazy_static! {
-    static ref PREC_CLIMBER: PrecClimber<Rule> = {
-        use Rule::*;
-        use Assoc::*;
-
-        PrecClimber::new(vec![
-            Operator::new(add, Left) | Operator::new(subtract, Left),
-            Operator::new(multiply, Left) | Operator::new(divide, Left),
-            Operator::new(power, Right)
-        ])
-    };
-}
-
-fn eval(expression: Pairs<Rule>) -> f64 {
-    PREC_CLIMBER.climb(
-        expression,
-        |pair: Pair<Rule>| match pair.as_rule() {
-            Rule::number => pair.as_str().parse::<f64>().unwrap(),
-            Rule::expr => eval(pair.into_inner()),
-            _ => unreachable!(),
-        },
-        |lhs: f64, op: Pair<Rule>, rhs: f64| match op.as_rule() {
-            Rule::add      => lhs + rhs,
-            Rule::subtract => lhs - rhs,
-            Rule::multiply => lhs * rhs,
-            Rule::divide   => lhs / rhs,
-            Rule::power    => lhs.powf(rhs),
-            _ => unreachable!(),
-        },
-    )
-}
+#[grammar = "../pest/grammar.pest"]
+pub struct GrammarParser;
 
 fn main() {
-    match CalculatorParser::parse(Rule::calculation, include_str!("../pest/input.txt")) {
+    match GrammarParser::parse(Rule::program, include_str!("../input.txt")) {
         Ok(tree) => {
-            println!("{}", eval(tree));
+            println!("{}", tree);
         },
         Err(err) => {
             eprintln!("Parse Error {}", err);
