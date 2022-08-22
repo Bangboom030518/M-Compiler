62cf00c58ae2aa5210d894aaae2b99bd732e70b5
Charlie Clift <bang.boom030518@gmail.com>
2022-08-22 08:36:09 +0100

    Finish bnf range extension

--- bnf/main.rs
+++ bnf/main.rs
@@ -1,7 +1,8 @@
+#![feature(iter_intersperse)]
 use bnf::Grammar;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::{fs, process};
+use std::process;
 
 lazy_static! {
     static ref COMMENTS_PATTERN: Regex = Regex::new(r"\(\*[\s\S]*?\*\)").unwrap();
@@ -14,32 +15,42 @@ fn error(message: &str) {
 }
 
 fn parse_bnf(input: &str) -> String {
-    let input = COMMENTS_PATTERN.replace_all(input, "rep").to_string();
-
-    for captures in RANGE_PATTERN.captures_iter(&input) {
-        let range = &captures["Range"];
-        range
+    let mut input = COMMENTS_PATTERN.replace_all(input, "").to_string();
+    for captures in RANGE_PATTERN.captures_iter(&input.clone()) {
+        let range: &str = &captures["Range"];
+        let expanded = range
             .chars()
             .collect::<Vec<char>>()
             .chunks(3)
             .map(|chunk| {
+                if chunk.len() != 3 || chunk[1] != '-' {
+                    error(&format!("Couldn't parse range {}", range))
+                };
                 (chunk[0], chunk[2])
-            });
+            })
+            .map(|pair| {
+                (pair.0..=pair.1)
+                    .map(|ch| format!("'{}'", ch))
+                    .intersperse("|".to_string())
+                    .collect::<String>()
+            })
+            .collect::<String>();
+        input = input.replace(&format!("[{}]", range), &expanded);
     }
     input
 }
 
 fn main() {
-    let input = parse_bnf(&fs::read_to_string("input.bnf").unwrap());
-    // print!("{}", input);
+    let input = parse_bnf(include_str!("../bnf/input.bnf"));
+    print!("{}", input);
 
-    let grammar: Grammar = input.parse().unwrap_or_else(|_| Grammar::new());
+    let grammar: Grammar = input.parse().unwrap();
 
     let sentence = "0";
 
     let mut parse_trees = grammar.parse_input(sentence);
     match parse_trees.next() {
         Some(parse_tree) => println!("{}", parse_tree),
-        _ => println!("Grammar could not parse sentence"),
+        None => println!("Grammar could not parse sentence"),
     }
 }
