2dc4db95ea96a73e58681bb9064666dd4cb45c74
Charlie Clift <bang.boom030518@gmail.com>
2022-08-24 11:31:08 +0100

    Rename keywords in grammar

--- pest/test.rs
+++ pest/test.rs
@@ -1,11 +1,11 @@
+#[cfg(test)]
 use core::panic;
-
 use super::*;
 
 #[test]
 fn add() {
     let tree = match parse("1 + 1;") {
-        Ok(tree) => tree,
+        Ok(tree) => print_tree(tree),
         Err(error) => panic!("Parse Error {}", error)
     };
     // assert_eq!(tree, 1)
