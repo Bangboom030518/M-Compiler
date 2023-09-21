69c319b2681f36a83f18a9a547a65b1f402232ea
Charlie Clift <bang.boom030518@gmail.com>
2023-09-21 18:05:17 +0100

    make tokenizer work
    Co-authored-by: AlanRandon <AlanRandon@users.noreply.github.com>

--- tokenizer/src/lib.rs
+++ tokenizer/src/lib.rs
@@ -1,20 +1,20 @@
+use itertools::Itertools;
+use std::iter::Peekable;
 use std::str::Chars;
 
-fn main() {
-    println!("Hello, world!");
-}
-
-enum Token {
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Token {
     Const,
     Function,
     Type,
     // TODO: ?
     // Enum,
+    Exponent,
     Union,
     Struct,
     Assignment,
-    Add,
-    Subtract,
+    Plus,
+    Minus,
     Multiply,
     Divide,
     Indent,
@@ -23,19 +23,110 @@ enum Token {
     Bang,
     Illegal,
     Ident(String),
+    Comment(String),
     // Parenthesised(Vec<Token>),
 }
 
-struct Tokenizer<'a>(Chars<'a>);
+pub struct Tokenizer<'a>(Peekable<Chars<'a>>);
+
+impl<'a> From<&'a str> for Tokenizer<'a> {
+    fn from(value: &'a str) -> Self {
+        Self(value.chars().peekable())
+    }
+}
+
+impl<'a> Tokenizer<'a> {
+    fn take_ident_or_keyword(&mut self, first_char: char) -> Token {
+        let ident: String = std::iter::once(first_char)
+            .chain(
+                self.0
+                    .peeking_take_while(|&ch| ch.is_alphanumeric() || ch == '_'),
+            )
+            .collect();
+
+        match ident.as_str() {
+            "const" => Token::Const,
+            "struct" => Token::Struct,
+            "union" => Token::Union,
+            "function" => Token::Function,
+            "type" => Token::Type,
+            _ => Token::Ident(ident),
+        }
+    }
+
+    fn take_comment_or_divide(&mut self) -> Token {
+        match self.0.peek() {
+            Some(&'/') => {
+                self.0.next();
+                Token::Comment(self.0.peeking_take_while(|&ch| ch != '\n').collect())
+            }
+            Some(&'*') => {
+                self.0.next();
+                let mut comment = String::new();
+                while let Some(ch) = self.0.next() {
+                    if ch == '*' && self.0.peek() == Some(&'/') {
+                        self.next();
+                        break;
+                    }
+                    comment.push(ch)
+                }
+                Token::Comment(comment)
+            }
+            _ => Token::Divide,
+        }
+    }
+}
 
 impl<'a> Iterator for Tokenizer<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
         let ch = self.0.next()?;
-        match ch {
-            ''
-        }
-        // Some(ch)
+        let token = match ch {
+            '+' => Token::Plus,
+            '-' => Token::Minus,
+            '.' => Token::Dot,
+            '\t' => Token::Indent,
+            '!' => Token::Bang,
+            '*' => {
+                if self.0.peek() == Some(&'*') {
+                    self.0.next();
+                    Token::Exponent
+                } else {
+                    Token::Multiply
+                }
+            }
+            '=' => {
+                if self.0.peek() == Some(&'=') {
+                    self.0.next();
+                    Token::Equality
+                } else {
+                    Token::Assignment
+                }
+            }
+            '/' => self.take_comment_or_divide(),
+            ch if ch.is_whitespace() => self.next()?,
+            ch if ch.is_alphabetic() || ch == '_' => self.take_ident_or_keyword(ch),
+            _ => Token::Illegal,
+        };
+        Some(token)
     }
+}
+
+#[test]
+fn tokenize_stuff() {
+    let input = "+ / * /*Hello World*/ const identifier_test";
+    let tokenizer: Tokenizer = input.into();
+    let tokens = tokenizer.collect_vec();
+    assert_eq!(
+        tokens,
+        vec![
+            Token::Plus,
+            Token::Divide,
+            Token::Multiply,
+            Token::Comment("Hello World".to_string()),
+            Token::Const,
+            Token::Ident("identifier_test".to_string())
+        ]
+    );
 }
