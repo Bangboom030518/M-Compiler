30465e9509fce1095915791ed54f7c8358d6a678
Charlie Clift <bang.boom030518@gmail.com>
2023-09-23 22:20:44 +0100

    add a bit more AST

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -2,12 +2,47 @@ use tokenizer::Tokenizer;
 
 pub struct Identifier(String);
 
+pub enum Type {
+    Identifier(Identifier)
+}
+
+// TODO: rename
+struct Field {
+    r#type: Type,
+    name: Identifier,
+}
+
+enum TypeDeclarationKind {
+    Struct,
+    Union,
+}
+
+struct TypeDeclaration {
+    fields: Vec<Field>,
+    declarations: Vec<TopLevelDeclaration>,
+    kind: TypeDeclarationKind,
+}
+
+enum Body {
+    Single(Expression),
+    Multiple(Vec<Expression>)
+}
+
+struct Function {
+    parameters: Vec<Field>,
+    return_type: Type,
+    body: Body
+}
+
+enum Expression {
+    Identifier(Identifier)
+}
+
 enum TopLevelDeclarationKind {
     Function(Function),
     Const(Expression),
     // TODO: ?
-    Struct(Struct),
-    Union(Union)
+    Type(TypeDeclaration),
 }
 
 struct TopLevelDeclaration {
